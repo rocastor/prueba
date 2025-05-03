822962d9745de74b3ee341e0f9cf3d569ea66548
rocastor <camilo.paezt@gmail.com>
2025-05-02 19:44:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,27 @@
-from datetime import datetime
+
 from utils.secop_scraper import buscar_licitaciones as buscar_licitaciones_secop1
 from utils.secop2_scraper import buscar_licitaciones as buscar_licitaciones_secop2
 from utils.mailer import enviar_resultados_por_correo
-from utils.drive_uploader import subir_a_drive
+from utils.drive_uploader import subir_a_drive_si_hay_resultados
+from datetime import datetime
 
-# Parámetros
 palabras_clave = ["papel", "computador", "resma", "silla", "mobiliario"]
-timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M")
-archivo_secop1 = f"secop1_{timestamp}.csv"
-archivo_secop2 = f"secop2_{timestamp}.csv"
+carpeta_drive_id = "TU_CARPETA_ID_AQUI"
 
-# Buscar en SECOP I
-resultados_secop1 = buscar_licitaciones_secop1(palabras_clave, archivo_secop1)
+timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M')
 
-# Buscar en SECOP II
-resultados_secop2 = buscar_licitaciones_secop2(palabras_clave, archivo_secop2)
+# Buscar licitaciones en SECOP I
+nombre_archivo_secop1 = f"secop1_resultados_{timestamp}.csv"
+resultados_secop1 = buscar_licitaciones_secop1(palabras_clave, nombre_archivo_secop1)
 
-# Si hay resultados, enviarlos por correo y subir a Drive
-archivos_enviar = []
+# Buscar licitaciones en SECOP II
+nombre_archivo_secop2 = f"secop2_resultados_{timestamp}.csv"
+resultados_secop2 = buscar_licitaciones_secop2(palabras_clave, nombre_archivo_secop2)
 
-if resultados_secop1:
-    archivos_enviar.append(archivo_secop1)
+# Enviar resultados por correo si hay
+enviar_resultados_por_correo(nombre_archivo_secop1, resultados_secop1)
+enviar_resultados_por_correo(nombre_archivo_secop2, resultados_secop2)
 
-if resultados_secop2:
-    archivos_enviar.append(archivo_secop2)
-
-if archivos_enviar:
-    enviar_resultados_por_correo(archivos_enviar)
-    subir_a_drive(archivos_enviar)
-else:
-    print("No se encontraron licitaciones para enviar.")
+# Subir a Drive solo si hay resultados
+subir_a_drive_si_hay_resultados(nombre_archivo_secop1, carpeta_drive_id)
+subir_a_drive_si_hay_resultados(nombre_archivo_secop2, carpeta_drive_id)
