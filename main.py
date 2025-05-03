e242c99a43603f9c9efb55aa272964b48f0740aa
rocastor <camilo.paezt@gmail.com>
2025-05-02 19:52:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
-
 from utils.secop_scraper import buscar_licitaciones as buscar_licitaciones_secop1
 from utils.secop2_scraper import buscar_licitaciones as buscar_licitaciones_secop2
 from utils.mailer import enviar_resultados_por_correo
 from utils.drive_uploader import subir_a_drive_si_hay_resultados
 from datetime import datetime
 
+# Parámetros
 palabras_clave = ["papel", "computador", "resma", "silla", "mobiliario"]
-carpeta_drive_id = "TU_CARPETA_ID_AQUI"
-
 timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M')
+archivo_secop1 = f"licitaciones_secop1_{timestamp}.csv"
+archivo_secop2 = f"licitaciones_secop2_{timestamp}.csv"
 
-# Buscar licitaciones en SECOP I
-nombre_archivo_secop1 = f"secop1_resultados_{timestamp}.csv"
-resultados_secop1 = buscar_licitaciones_secop1(palabras_clave, nombre_archivo_secop1)
+# Ejecutar búsqueda
+resultados_secop1 = buscar_licitaciones_secop1(palabras_clave, archivo_secop1)
+resultados_secop2 = buscar_licitaciones_secop2(palabras_clave, archivo_secop2)
 
-# Buscar licitaciones en SECOP II
-nombre_archivo_secop2 = f"secop2_resultados_{timestamp}.csv"
-resultados_secop2 = buscar_licitaciones_secop2(palabras_clave, nombre_archivo_secop2)
+# Subir a Google Drive (si hay resultados)
+carpeta_drive_id = "1mDXUO0tYyIAYAgfpUctnhWvXZyG1aTff"
+subir_a_drive_si_hay_resultados(archivo_secop1, carpeta_drive_id)
+subir_a_drive_si_hay_resultados(archivo_secop2, carpeta_drive_id)
 
-# Enviar resultados por correo si hay
-enviar_resultados_por_correo(nombre_archivo_secop1, resultados_secop1)
-enviar_resultados_por_correo(nombre_archivo_secop2, resultados_secop2)
-
-# Subir a Drive solo si hay resultados
-subir_a_drive_si_hay_resultados(nombre_archivo_secop1, carpeta_drive_id)
-subir_a_drive_si_hay_resultados(nombre_archivo_secop2, carpeta_drive_id)
+# Enviar por correo (si hay resultados)
+enviar_resultados_por_correo([archivo_secop1, archivo_secop2])
