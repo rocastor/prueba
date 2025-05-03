8c4fd8dac0a8f3714a64376ea3ff1e178a5fe78b
rocastor <camilo.paezt@gmail.com>
2025-05-02 20:05:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
+
 from utils.secop_scraper import buscar_licitaciones as buscar_licitaciones_secop1
 from utils.secop2_scraper import buscar_licitaciones as buscar_licitaciones_secop2
 from utils.mailer import enviar_resultados_por_correo
 from utils.drive_uploader import subir_a_drive_si_hay_resultados
 from datetime import datetime
 
-# Parámetros
+# Palabras clave a buscar
 palabras_clave = ["papel", "computador", "resma", "silla", "mobiliario"]
-timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M')
-archivo_secop1 = f"licitaciones_secop1_{timestamp}.csv"
-archivo_secop2 = f"licitaciones_secop2_{timestamp}.csv"
 
-# Ejecutar búsqueda
+# Nombres de archivos
+fecha_hora = datetime.now().strftime('%Y-%m-%d_%H-%M')
+archivo_secop1 = f"licitaciones_secop1_{fecha_hora}.csv"
+archivo_secop2 = f"licitaciones_secop2_{fecha_hora}.csv"
+
+# Carpeta de Drive (ID de tu carpeta en Google Drive)
+carpeta_drive_id = "1JWAw2wEy_NjI7bMrBdjIkS30jByO7Pyd"
+
+# Buscar licitaciones en SECOP I y II
 resultados_secop1 = buscar_licitaciones_secop1(palabras_clave, archivo_secop1)
 resultados_secop2 = buscar_licitaciones_secop2(palabras_clave, archivo_secop2)
 
-# Subir a Google Drive (si hay resultados)
-carpeta_drive_id = "1mDXUO0tYyIAYAgfpUctnhWvXZyG1aTff"
+# Subir a Drive solo si hay resultados
 subir_a_drive_si_hay_resultados(archivo_secop1, carpeta_drive_id)
 subir_a_drive_si_hay_resultados(archivo_secop2, carpeta_drive_id)
 
-# Enviar por correo (si hay resultados)
-enviar_resultados_por_correo([archivo_secop1, archivo_secop2])
+# Enviar por correo si hay resultados
+enviar_resultados_por_correo(archivo_secop1)
+enviar_resultados_por_correo(archivo_secop2)
